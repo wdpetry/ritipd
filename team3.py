d6cf5b36bd8eb7a9a07bfdef6352c6ac95f21c8b
Michael <mrada6@gmail.com>
2018-01-23 10:11:10 -0500

    Mike Rad updated his team

--- team3.py
+++ team3.py
@@ -26,7 +26,8 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    if len(my_history)<10:
+        return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):

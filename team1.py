1d1c9828412dd52137cbaa369303eb96eb4a1a89
schaefferc <schaefferc@newberg.k12.or.us>
2018-01-23 10:03:54 -0500

    Schaeffer Team Upload 1
    My first strategy

--- team1.py
+++ team1.py
@@ -10,23 +10,48 @@ team_name = 'The name the team gives to itself' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
-def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
+#def move(my_history, their_history, my_score, their_score):
+#    ''' Arguments accepted: my_history, their_history are strings.
+#    my_score, their_score are ints.
+#    
+#    Make my move.
+#    Returns 'c' or 'b'. 
+#    '''
+#
+#    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
+#    # their_history: a string of the same length as history, possibly empty. 
+#    # The first round between these two players is my_history[0] and their_history[0].
+#    # The most recent round is my_history[-1] and their_history[-1].
+#    
+#    # Analyze my_history and their_history and/or my_score and their_score.
+#    # Decide whether to return 'c' or 'b'.
+#    
+#    return 'c'
 
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
+
+def move(my_history, their_history, my_score, their_score):
+    redeem=0#create a redeem value 
     
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+    for i in range(0,len(my_history)-1): 
+        if (my_history[-i] == 'c'):     
+            redeem +=1   #based on the number of times they've colluded with me.
+            
+    if len(my_history)==0: # It's the first round; collude.
+        return 'c'
+        
+    elif my_history[-1]=='c' and their_history[-1]=='b':
+        return 'b' # Betray if severely punished last time
+        
+    elif their_history[-2]=='c' or their_history[-3]=='c':
+        return 'c' # collude if they've colluded recently
+        
+    elif ( (redeem / len(my_history) )*100>30 ): 
+        return 'c'# collude if they've colluded more than 30% of the time
+
+    else:
+        return 'b' # otherwise betray
+
+
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
